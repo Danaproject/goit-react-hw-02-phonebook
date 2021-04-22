0947ffcbccc1da1b81de7321a4cd6f304630486d
Danaproject <decoratio@gmail.com>
2021-04-22 21:05:45 +0200

    +propTypes

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,18 +1,9 @@
+import ContactItem from './ContactItem'
 import './ContactList.scss';
 
 const ContactList = ({ contacts, onDeleteContact }) => (
-    <ul className="ContactList">
-        {contacts.map(({ id, name, number }) => (
-        <li key={id} className="ContactList__item">
-            <p className="ContactList__text">{name}: &#x2121; {number}</p>
-        <button
-            type="button"
-            className="ContactList__btn"
-            onClick={() => onDeleteContact(id)}>
-            &#128465;
-        </button>
-      </li>
-    ))}
+  <ul className="ContactList">
+    <ContactItem contacts={contacts} onDeleteContact={onDeleteContact} />
   </ul>
 );
 
